857b43e1d83e3ad7879a312ea2b78b184f2f25c1
Yura <yura04091@gmail.com>
2021-09-14 14:24:37 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,38 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
 
-const reducer = (state=0, action) => {
-  switch(action.type) {
-    case 'INC':
-      return state + 1;
-      case 'DEC':
-        return state - 1;
-      case 'RES' :
-        return state = 0;
-      default:
-        return state;
 
-  }
-}
+
 
 const store = createStore(reducer);
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = () => ({type: 'RES'});
 
-document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc())
-});
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec())
-});
-document.getElementById('res').addEventListener('click', () => {
-  store.dispatch(rnd())
-});
 
-const update = () => {
-  document.getElementById('count').textContent=store.getState();
-};
 
-store.subscribe(update);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  , document.getElementById('root'));
+
+
+
 
 
