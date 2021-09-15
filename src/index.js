15603ee4530790510c6f83e107ec507de3c3c6f3
Yura <yura04091@gmail.com>
2021-09-15 14:12:02 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/app';
+import App from './components/app';
 import './style.css';
 
 const store = createStore(reducer);
